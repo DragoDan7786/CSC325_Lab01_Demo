2d5012245a86e198ef06eb4d4791c16aa2227bf0
soblab <soblab@Whit209-12.it.farmingdale.edu>
2022-02-01 14:52:55 -0500

    Initial Repo Commit

---
+++ src/main/java/com/mycompany/csc_325_lab01_demo/NewClass.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.csc_325_lab01_demo;
+
+public class NewClass {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!!!");
+    
+    }
+}
